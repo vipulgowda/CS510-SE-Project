ba59fea298d6eba0323b66af7d6edf9b749f3066
Vipul Puttaswamy Gowda <vipulp@pdx.edu>
2025-03-16 15:55:56 -0700

    backend integration of SSO

--- src/app.py
+++ src/app.py
@@ -3,12 +3,13 @@ from flask_cors import CORS
 from config.settings import Config
 from core.database import init_db
 from api.v1.routes import api
+import os
 
 # Initialize Flask app
 app = Flask(__name__)
 CORS(app)
 app.config.from_object(Config)
-
+app.secret_key = os.getenv("FLASK_SECRET_KEY", "hello")
 # Initialize database
 init_db(app)
 
